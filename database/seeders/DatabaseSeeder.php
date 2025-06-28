84fa7a6c06eb380b20cbd19ad88eaf5f58f2b3dd
Lenenba <bilitikjulesroger@yahoo.fr>
2025-06-28 14:24:22 -0400

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Database\Seeders\DevSeeder;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
